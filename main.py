e3fe7bd3ad0e6205c4ebefd6a8caad7731afeed9
Linkeroftime <162757323+Linkeroftime@users.noreply.github.com>
2024-06-27 11:33:03 -0400

    Update main.py
    added database functionality with SQLAlchemy ORM and SQLite3

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from openai import OpenAI
 
 client = OpenAI(api_key='your-api-key')
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-class Movie:
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    releaseYear = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String(80), nullable=False)
+    coverUrl = db.Column(db.String(200), nullable=False)
+    summary = db.Column(db.String(500), nullable=False)
+
+
     def __init__(self, title, releaseYear, genre, coverUrl):
         self.title = title
         self.releaseYear = releaseYear
@@ -27,27 +39,32 @@ class Movie:
         summary = response.choices[0].message.content.strip()
         return summary
 
-darkKnight = Movie(
-    "The Dark Knight", 
-    2008, 
-    "Action", 
-    "https://irs.www.warnerbros.com/keyart-jpeg/movies/media/browser/the_dark_knight_key_art.jpg"
-)
+def add_hardcoded_movies():
+    if Movie.query.count() == 0:
+        darkKnight = Movie(
+            "The Dark Knight",
+            2008,
+            "Action",
+            "https://irs.www.warnerbros.com/keyart-jpeg/movies/media/browser/the_dark_knight_key_art.jpg"
+        )
 
 
 
-shawshankRedemption = Movie(
-    "The Shawshank Redemption", 
-    1994, 
-    "Drama", 
-    "https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i"
-)
+        shawshankRedemption = Movie(
+            "The Shawshank Redemption",
+            1994,
+            "Drama",
+            "https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i"
+        )
 
-movies = [darkKnight, shawshankRedemption]
 
+ db.session.add(darkKnight)
+        db.session.add(shawshankRedemption)
+        db.session.commit()
 
 @app.route('/getmovies', methods=['GET'])
 def getMovies():
+    movies = Movie.query.all()
     movieData = []
     for movie in movies:
         movieData.append({
@@ -61,9 +78,9 @@ def getMovies():
 
 @app.route('/getmoviesbyyear/<int:year>', methods=['GET'])
 def getMoviesByYear(year):
+    movies = Movie.query.filter_by(releaseYear=year).all()
     movieData = []
     for movie in movies:
-        if movie.releaseYear == year:
            movieData.append({
                 "title":movie.title,
                 "releaseYear":movie.releaseYear,
@@ -76,7 +93,10 @@ def getMoviesByYear(year):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        add_hardcoded_movies()
+    app.run(debug=True, host='0.0.0.0')
 
 
 '''
